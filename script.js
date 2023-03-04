72c096764819944e34cd4896347c8dc6325bc1e9
kvakusya <117990154+kvakusya@users.noreply.github.com>
2023-03-04 13:35:39 +0300

    firstsite

---
+++ script.js
@@ -0,0 +1,10 @@
+var paragraph = document.querySelector('.headslide2');
+var headslide2 = paragraph.CDATA_SECTION_NODE.link;
+var a=document.createElement('a');
+var a=link;
+var bChild=paragraph.removeChild(headslide2);
+paragraph.insertBefore(a, paragraph.cChild);
+a.innerHTML=bChild.outerHTML;
+
+
+
